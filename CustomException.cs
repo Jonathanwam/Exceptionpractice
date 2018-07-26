a04fd2847f58022cead96d0eda0de787c48f3929
Jonathan Wambach <jonathanwam@gmail.com>
2018-07-26 10:25:46 -0400

    first commit

---
+++ CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exceptionpractice
+{
+    [Serializable]
+    public class CustomException : Exception
+    {
+        public CustomException() :
+            base("My custom exception...")
+        {
+        }
+
+        public CustomException(String message) :
+            base(message)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exceptionpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 0;
+            int[] nums = new int[3]{1,2,3};
+            try
+            {
+                throw new CustomException();
+                throw new CustomException("This is even more customized!");
+                Console.Write($"{i/j}");
+                Console.Write("Out of index num?" + nums[5] + "\n");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.Write("You can't divide by zero... " + ex + "\n");
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Got this more generic exception... " + ex + "\n");
+            }
+            finally
+            {
+                Console.Write("Finally! \n");
+            }
+
+            Console.Write("Out of the try-catch");
+        }
+    }
+}
